689f8794c1281a532e4dd04cc6a07c84c8963e89
PavelShoplik <pavel.shoplik@yandex.ru>
2025-12-07 19:53:09 +0300

    #3 - Small changes

--- feature/vitals/data/src/main/kotlin/ru/health/stream/feature/vitals/data/repository/MeasurementRepository.kt
+++ feature/vitals/data/src/main/kotlin/ru/health/stream/feature/vitals/data/repository/MeasurementRepository.kt
@@ -1,13 +1,22 @@
 package ru.health.stream.feature.vitals.data.repository
 
 import kotlinx.datetime.Instant
-import ru.health.stream.feature.vitals.data.model.VitalMeasurement
+import ru.health.stream.feature.vitals.data.model.HealthMeasurement
 import kotlin.reflect.KClass
 import kotlin.time.Duration
 
 interface MeasurementRepository {
 
-    fun <T: VitalMeasurement> getMeasurementsByRange(from: Instant, to: Instant, type: KClass<T>): List<T>
-    fun <T: VitalMeasurement> getMeasurementsByDuration(duration: Duration, type: KClass<T>): List<T>
-    fun <T: VitalMeasurement> createMeasurement(measurement: T): Result<T>
+    fun <T : HealthMeasurement> getMeasurementsByRange(
+        from: Instant,
+        to: Instant,
+        type: KClass<T>
+    ): List<T>
+
+    fun <T : HealthMeasurement> getMeasurementsByDuration(
+        duration: Duration,
+        type: KClass<T>
+    ): List<T>
+
+    fun <T : HealthMeasurement> createMeasurement(measurement: T): Result<T>
 }
