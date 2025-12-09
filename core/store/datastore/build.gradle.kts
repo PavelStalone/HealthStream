102638fea16fd2b2e4fd62d87cb1d68193b38a12
PavelStalone <69467433+PavelStalone@users.noreply.github.com>
2025-12-09 22:00:32 +0300

    #10 - [DataStore] Implement DataStore (#12)

--- core/store/datastore/build.gradle.kts
+++ core/store/datastore/build.gradle.kts
@@ -1,7 +1,13 @@
 plugins {
     id("health.stream.android.library")
+    id("health.stream.android.hilt")
 }
 
 android {
     namespace = "ru.health.stream.core.store.datastore"
+}
+
+dependencies {
+    implementation(libs.androidx.datastore.core)
+    implementation(libs.androidx.datastore.preferences)
 }
