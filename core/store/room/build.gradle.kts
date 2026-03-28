6bfca9aae63896bc8d3cddfa22015026f05a73ae
Alexander Gorodnikov <aleks_gor2000@mail.ru>
2026-03-28 13:46:24 +0300

    Create Room Gradle Convention Plugin and remove all compiled plugins from the root gradle module

--- core/store/room/build.gradle.kts
+++ core/store/room/build.gradle.kts
@@ -1,25 +1,15 @@
 plugins {
     id("android.library")
     id("android.hilt")
+    id("kotlin.room")
     id("kotlin.serialization")
-
-    alias(libs.plugins.androidx.room)
 }
 
 android {
     namespace = "ru.health.stream.core.store.room"
 }
 
-room {
-    schemaDirectory("$projectDir/schemas")
-}
-
 dependencies {
-    ksp(libs.androidx.room.compiler)
-
-    implementation(libs.androidx.room.ktx)
-    implementation(libs.androidx.room.runtime)
-
     implementation(projects.core.store)
     implementation(projects.core.monitor)
 
